5034e721470704173dde385ff7910f79ab002a14
hatiwaredesu <tomo17172424@icloud.com>
2025-07-25 15:16:06 +0900

    ナカノ開始点

---
+++ agileD/src/main/java/repository/DishLogRepository.java
@@ -0,0 +1,40 @@
+package repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DishLogRepository {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	//さくら
+	
+	
+	
+	
+	//ともひと
+	
+	
+	
+	
+	//まな
+	
+	
+	
+	
+	
+	//れいれい
+	
+	
+	
+	
+	
+	//みんひょく
+	
+	
+	
+	
+
+}
